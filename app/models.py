d257c786e45370f06e406e8d8dc8ecafe5e2b16e
Matheus Rabelo <matheusfreirerabelo@gmail.com>
2021-10-26 05:03:09 -0300

    Add project model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    class Meta:
+       abstract = True
+
+
+class Project(BaseModel):
+    title = models.CharField(max_length=180)
+    description = models.TextField(null=True, blank=True)
+    url = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
